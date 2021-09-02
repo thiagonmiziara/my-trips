b3b461492d1978d9505df060c9002bab59495b4c
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-02 20:20:21 -0300

    add react-leaflet and component map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import Main from 'components/Main'
+import dynamic from 'next/dynamic'
+
+// eslint-disable-next-line prettier/prettier
+//a função dynamic serve para importar lib que não usa server side rendering
+const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Main />
+  return <Map />
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -33,7 +33,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell,'Open Sans', 'Helvetica Neue', sans-serif
   }
 
   p,a{
