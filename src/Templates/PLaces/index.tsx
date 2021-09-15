34dba5fcf95fc0bbb6d6cc99b0b7f30b5b0f1097
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-15 19:16:23 -0300

    styles place template

--- src/Templates/PLaces/index.tsx
+++ src/Templates/PLaces/index.tsx
@@ -1,16 +1,35 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
 import { PlaceTemplateProps } from 'models'
+
+import * as S from './styles'
 
 export default function PlaceTemplate({ place }: PlaceTemplateProps) {
   return (
     <>
-      <h1>{place.name}</h1>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
 
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
 
-      {place.gallery.map((image, index) => {
-        // eslint-disable-next-line @next/next/no-img-element
-        return <img src={image.url} alt={place.name} key={`photo-${index}`} />
-      })}
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description.html }}
+          />
+
+          <S.Gallery>
+            {place.gallery.map((image, index) => {
+              return (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img src={image.url} alt={place.name} key={`photo-${index}`} />
+              )
+            })}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
--- src/Templates/PLaces/styles.ts
+++ src/Templates/PLaces/styles.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components'
 
-export const Div = styled.div``
+export const Wrapper = styled.div`
+  padding: var(--large) var(--medium);
+`
+export const Container = styled.section`
+  max-width: var(--container);
+  margin: 0 auto;
+`
+
+export const Heading = styled.h1`
+  font-size: var(--large);
+  color: var(--highlight);
+  margin-bottom: var(--medium);
+`
+
+export const Body = styled.div`
+  margin-bottom: var(--large);
+
+  p {
+    margin-bottom: var(--medium);
+  }
+`
+
+export const Gallery = styled.div`
+  display: grid;
+  grid-gap: var(--medium);
+
+  img {
+    max-width: var(--container);
+  }
+`
