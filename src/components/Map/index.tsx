7a46974b66044f9868536c2f3797959fa0030d95
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-22 20:02:29 -0300

    add minZoom and maxZomm leaflet

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -30,6 +30,8 @@ const Map = ({ places }: MapProps) => {
         center={[0, 0]}
         zoom={3}
         style={{ height: '100%', width: '100%' }}
+        minZoom={3}
+        maxZoom={7}
       >
         <CustomTileLayer />
         {places?.map(({ id, slug, name, location }) => {
